2c0d55c56fef09207fb2482912d3ad989f97a018
julianleelinker <julianlee@linkernetworks.com>
2021-05-27 14:06:48 +0800

    remove install fonts

--- install.sh
+++ install.sh
@@ -3,12 +3,8 @@ add-apt-repository ppa:kelleyk/emacs
 apt update
 apt install emacs27
 
-# install my emacs settings and fonts
-mkdir ~/.fonts
-cp ~/.spacemacs.d/CascadiaMono-* ~/.fonts
-fc-cache -f -v
-
 # install spacemacs
+rm -rf ~/emacs.d
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 # add some alias to bashrc
