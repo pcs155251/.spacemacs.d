98887f70512bb907fdcb7ecc4329c10afe0145a8
julianleelinker <julianlee@linkernetworks.com>
2021-05-27 14:47:24 +0800

    done install script

--- init.el
+++ init.el
@@ -565,6 +565,10 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
+ '(custom-safe-themes
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
  '(evil-want-Y-yank-to-eol nil)
  '(hl-todo-keyword-faces
    '(("TODO" . "#dc752f")
--- install.sh
+++ install.sh
@@ -8,3 +8,9 @@ rm -rf ~/emacs.d
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 # add some alias to bashrc
+echo "emacs --daemon" >> ~/.bashrc
+echo "alias emc='emacsclient -c -a=\"\"'" >> ~/.bashrc
+echo "alias emct='emacsclient -t -c -a=\"\"'" >> ~/.bashrc
+
+# if terminal color is wierd, try M-x load-theme spacemacs-dark  
+
