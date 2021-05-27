3c32a5139e0bd9574d8ba7d8f87d9fdcbac38096
julianleelinker <julianlee@linkernetworks.com>
2021-05-27 19:26:06 +0800

    optimize settings

--- init.el
+++ init.el
@@ -550,7 +550,21 @@ dump.")
 This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
-before packages are loaded.")
+before packages are loaded."
+  (setq org-todo-keywords
+       '((sequence "TODO(t)" "PROGRESS(p)" "NEXT(n)" "WAITING(w)" "SOMEDAY(s)" "|" "DONE(d)" "CANCELLED(c)")))
+
+  (setq org-tag-alist '(
+		                    ("@laptop" . ?l)
+                        ("@office" . ?o)
+		                    ("@home" . ?h)
+		                    ("@jia" . ?j)
+		                    ("@free" . ?f)
+		                    ("ask" . ?f)
+		                    ("think" . ?t)
+		                    ("review". ?r))
+                       )
+)
 
 
 ;; Do not write anything past this comment. This is where Emacs will
--- install.sh
+++ install.sh
@@ -4,13 +4,14 @@ apt update
 apt install emacs27
 
 # install spacemacs
-rm -rf ~/emacs.d
+rm -rf ~/.emacs.d
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 # add some alias to bashrc
 echo "emacs --daemon" >> ~/.bashrc
 echo "alias emc='emacsclient -c -a=\"\"'" >> ~/.bashrc
 echo "alias emct='emacsclient -t -c -a=\"\"'" >> ~/.bashrc
+source ~/.bashrc
 
 # if terminal color is wierd, try M-x load-theme spacemacs-dark  
 
